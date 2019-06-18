42b449e6596b9023c07ca4c983f428431e9668da
Mennovdjagt <43231296+Mennovdjagt@users.noreply.github.com>
2019-06-18 05:41:34 -0700

    added a few functions I want to implement

--- NRF24.hpp
+++ NRF24.hpp
@@ -10,7 +10,15 @@ protected:
 
 public:
     
-    NRF24( uint16_t ce, uint16_t csn);     
+    NRF24( uint16_t ce, uint16_t csn); 
+	
+    uint8_t read_register(uint8_t reg);
+	
+    uint8_t write_register(uint8_t register, uint8_t value);
+	
+    void setChannel(uint8_t channel);
+	
+    uint8_t getChannel();
 
 };
 
