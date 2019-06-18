b56bda6c230cf1e6e92f1a53c5f1c36d5d620d7e
Mennovdjagt <43231296+Mennovdjagt@users.noreply.github.com>
2019-06-18 05:30:41 -0700

    added the class NRF24 with constructor

--- NRF24.hpp
+++ NRF24.hpp
@@ -1,1 +1,17 @@
-//empty
+#ifndef NRF24_HPP
+#define NRF24_HPP
+
+#include "hwlib.hpp"
+
+class NRF24 {
+protected:
+	uint16_t ce;
+	uint16_t csn;
+
+public:
+    
+    NRF24( uint16_t ce, uint16_t csn);     
+
+};
+
+#endif // NRF24_HPP
