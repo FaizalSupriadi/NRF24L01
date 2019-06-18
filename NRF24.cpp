40cd3ef598e480778d92e5c0b35148b81a726fcc
Mennovdjagt <43231296+Mennovdjagt@users.noreply.github.com>
2019-06-18 16:48:22 +0200

    implemented the basic functions
    implemented the functions I created in the .hpp file, the functions read_register and write_register are very important, because without them I can't create other functions just like you can see in the function setChannel and getChannel they need the read and write register functions to be able to do anything with the NRF24L01.

--- NRF24.cpp
+++ NRF24.cpp
@@ -1,1 +1,25 @@
-//empty
+#include "NRF24.hpp"
+#include "NRF24_register.hpp"
+#include "hwlib.hpp"
+
+NRF24::NRF24( uint16_t ce, uint16_t csn ):
+   ce( ce ), csn( csn )
+{} 
+	
+uint8_t NRF24::read_register( uint8_t reg ){
+   //use SPI to read a message
+}
+	
+uint8_t NRF24::write_register( uint8_t register, uint8_t value ){
+   //use SPI to write a message ( R_REGISTER | ( 0x1F & reg ) )
+   //verstuur daarna nog een keer via SPI een bericht maar dan alleen de value die je wilt schrijven
+}
+	
+void NRF24::setChannel( uint8_t channel ){
+   channel = ( channel < 125 ) ? channel : 125;
+   write_register( RF_CH, channel );
+}
+
+uint8_t NRF24::getChannel(){
+   return read_register( RF_CH );
+}
