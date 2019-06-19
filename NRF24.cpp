6281a94bdd48fd40895979a202b6d3e64c7c22c3
Mennovdjagt <43231296+Mennovdjagt@users.noreply.github.com>
2019-06-19 02:15:05 -0700

    changed read and write register
    trying to be able to read and write a register, I temporary return 1 at the write_register, because I do not know yet how to return a value with hwlib write functions.

--- NRF24.cpp
+++ NRF24.cpp
@@ -2,20 +2,21 @@
 #include "NRF24_register.hpp"
 #include "hwlib.hpp"
 
-NRF24::NRF24( uint16_t ce, uint16_t csn ):
-   ce( ce ), csn( csn )
+NRF24::NRF24( hwlib::spi_bus & bus, uint16_t ce, uint16_t csn ):
+   bus( bus ), ce( ce ), csn( csn )
 {} 
 	
 uint8_t NRF24::read_register( uint8_t reg ){
    uint8_t result;
-   //use SPI to read a message
-   result = readSPI(NOP);
+   bus.write( R_REGISTER | ( 0x1F & reg ) );
+   result = bus.read_byte(NOP);
    return result;
 }
 	
 uint8_t NRF24::write_register( uint8_t register, uint8_t value ){
-   //use SPI to write a message ( R_REGISTER | ( 0x1F & reg ) )
-   //verstuur daarna nog een keer via SPI een bericht maar dan alleen de value die je wilt schrijven
+   bus.write( R_REGISTER | ( 0x1F & reg ) );
+   bus.write(value);
+   return 1;
 }
 	
 void NRF24::setChannel( uint8_t channel ){
