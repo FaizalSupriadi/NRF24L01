07ca90bcabb9c1080bd07d5a12365c0994b7a2a8
Mennovdjagt <43231296+Mennovdjagt@users.noreply.github.com>
2019-06-19 05:20:25 -0700

    Update NRF24.cpp
    not yet working, gives back a value of 14 but doesn't change the value

--- NRF24.cpp
+++ NRF24.cpp
@@ -2,20 +2,28 @@
 #include "NRF24_register.hpp"
 #include "hwlib.hpp"
 
+//bus.transaction( sel ).write( write_buffer );
+
 NRF24::NRF24( hwlib::spi_bus & bus, uint16_t ce, uint16_t csn ):
    bus( bus ), ce( ce ), csn( csn )
 {} 
 	
 uint8_t NRF24::read_register( uint8_t reg ){
    uint8_t result;
-   bus.write( R_REGISTER | ( 0x1F & reg ) );
-   result = bus.read_byte(NOP);
-   return result;
+   auto cs = hwlib::target::pin_out( hwlib::target::pins::d11 );
+   bus.transaction( cs ).write( R_REGISTER | ( 0x1F & reg ) );
+   result = bus.transaction( cs ).read_byte();
+   if(result > 0){
+      return result;
+   }else{
+      return 255;
+   }
 }
 	
-uint8_t NRF24::write_register( uint8_t register, uint8_t value ){
-   bus.write( R_REGISTER | ( 0x1F & reg ) );
-   bus.write(value);
+uint8_t NRF24::write_register( uint8_t reg, uint8_t value ){
+   auto cs = hwlib::target::pin_out( hwlib::target::pins::d11 );
+   bus.transaction( cs ).write( W_REGISTER | ( 0x1F & reg ) );
+   bus.transaction( cs ).write(value);
    return 1;
 }
 	
@@ -27,3 +35,4 @@ void NRF24::setChannel( uint8_t channel ){
 uint8_t NRF24::getChannel(){
    return read_register( RF_CH );
 }
+
