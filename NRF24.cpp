b56db01bcdc3b297ef4b36d78ed81f29eac0d9a4
Mennovdjagt <43231296+Mennovdjagt@users.noreply.github.com>
2019-06-20 12:50:16 +0200

    fixed the bug
    because the NRF uses the select pin when it is low instead of high it didn't work, so I have manually set the pin to low if it is used and high if done. Still need to fix the fact that the code is ugly

--- NRF24.cpp
+++ NRF24.cpp
@@ -1,30 +1,35 @@
 #include "NRF24.hpp"
 #include "NRF24_register.hpp"
 #include "hwlib.hpp"
-
-//bus.transaction( sel ).write( write_buffer );
+#include <array>
 
 NRF24::NRF24( hwlib::spi_bus & bus, uint16_t ce, uint16_t csn ):
    bus( bus ), ce( ce ), csn( csn )
 {} 
 	
-uint8_t NRF24::read_register( uint8_t reg ){
-   uint8_t result;
+uint8_t NRF24::read_register( uint8_t reg){
+   uint8_t result = 0;
    auto cs = hwlib::target::pin_out( hwlib::target::pins::d11 );
-   bus.transaction( cs ).write( R_REGISTER | ( 0x1F & reg ) );
-   result = bus.transaction( cs ).read_byte();
+   auto cs1 = hwlib::target::pin_out( hwlib::target::pins::d8 );
+   cs.write(0); cs.flush();
+   bus.transaction( cs1 ).write( R_REGISTER | ( 0x1F & reg ) );
+   result = bus.transaction( cs1 ).read_byte();
+   cs.write(1); cs.flush();
    if(result > 0){
       return result;
-   }else{
-      return 255;
+   }else{ 
+      return 255; 
    }
 }
 	
-uint8_t NRF24::write_register( uint8_t reg, uint8_t value ){
+void NRF24::write_register( uint8_t reg, uint8_t value ){
    auto cs = hwlib::target::pin_out( hwlib::target::pins::d11 );
-   bus.transaction( cs ).write( W_REGISTER | ( 0x1F & reg ) );
-   bus.transaction( cs ).write(value);
-   return 1;
+   auto cs1 = hwlib::target::pin_out( hwlib::target::pins::d8 );
+   cs.write(1); cs.flush();
+   cs.write(0); cs.flush();
+   bus.transaction( cs1 ).write( (W_REGISTER | ( 0x1F & reg )) );
+   bus.transaction( cs1 ).write(value);
+   cs.write(1); cs.flush();
 }
 	
 void NRF24::setChannel( uint8_t channel ){
@@ -34,5 +39,17 @@ void NRF24::setChannel( uint8_t channel ){
 
 uint8_t NRF24::getChannel(){
    return read_register( RF_CH );
+}
+
+void NRF24::csn_LOW(){
+   auto csn = hwlib::target::pin_out( hwlib::target::pins::d11 );
+   csn.write( 0 );
+   csn.flush();
+}
+
+void NRF24::ce_LOW(){
+   auto ce = hwlib::target::pin_out( hwlib::target::pins::d10 );
+   ce.write( 0 );
+   ce.flush();
 }
 
