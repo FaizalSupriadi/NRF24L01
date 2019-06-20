fbdf1f15b77d4e85e5c473bc45e3f29b79f8e12c
Mennovdjagt <43231296+Mennovdjagt@users.noreply.github.com>
2019-06-20 08:39:43 -0700

    cleaned the functions
    I cleaned the functions from pieces of code that are unnecessary.
    I also made sure that the class is given the pin_out from csn and ce, so I don't need to create them every time.
    I added pin_out_dummies for the transaction, because csn needs to be low to start a transaction, might change some of that because it doesn't look nice.

--- NRF24.cpp
+++ NRF24.cpp
@@ -3,33 +3,23 @@
 #include "hwlib.hpp"
 #include <array>
 
-NRF24::NRF24( hwlib::spi_bus & bus, uint16_t ce, uint16_t csn ):
+NRF24::NRF24( hwlib::spi_bus & bus, hwlib::pin_out & ce, hwlib::pin_out & csn ):
    bus( bus ), ce( ce ), csn( csn )
 {} 
 	
 uint8_t NRF24::read_register( uint8_t reg){
-   uint8_t result = 0;
-   auto cs = hwlib::target::pin_out( hwlib::target::pins::d11 );
-   auto cs1 = hwlib::target::pin_out( hwlib::target::pins::d8 );
-   cs.write(0); cs.flush();
-   bus.transaction( cs1 ).write( R_REGISTER | ( 0x1F & reg ) );
-   result = bus.transaction( cs1 ).read_byte();
-   cs.write(1); cs.flush();
-   if(result > 0){
-      return result;
-   }else{ 
-      return 255; 
-   }
+   set_csn(0);
+   bus.transaction( hwlib::pin_out_dummy ).write( R_REGISTER | ( 0x1F & reg ) );
+   uint8_t result = bus.transaction( hwlib::pin_out_dummy ).read_byte();
+   set_csn(1);
+   return result;
 }
 	
 void NRF24::write_register( uint8_t reg, uint8_t value ){
-   auto cs = hwlib::target::pin_out( hwlib::target::pins::d11 );
-   auto cs1 = hwlib::target::pin_out( hwlib::target::pins::d8 );
-   cs.write(1); cs.flush();
-   cs.write(0); cs.flush();
-   bus.transaction( cs1 ).write( (W_REGISTER | ( 0x1F & reg )) );
-   bus.transaction( cs1 ).write(value);
-   cs.write(1); cs.flush();
+   set_csn(0);
+   bus.transaction( hwlib::pin_out_dummy ).write( (W_REGISTER | ( 0x1F & reg )) );
+   bus.transaction( hwlib::pin_out_dummy ).write(value);
+   set_csn(1);
 }
 	
 void NRF24::setChannel( uint8_t channel ){
@@ -41,15 +31,12 @@ uint8_t NRF24::getChannel(){
    return read_register( RF_CH );
 }
 
-void NRF24::csn_LOW(){
-   auto csn = hwlib::target::pin_out( hwlib::target::pins::d11 );
-   csn.write( 0 );
+void NRF24::set_csn( bool x ){
+   csn.write( x );
    csn.flush();
 }
 
-void NRF24::ce_LOW(){
-   auto ce = hwlib::target::pin_out( hwlib::target::pins::d10 );
-   ce.write( 0 );
+void NRF24::set_ce( bool x ){
+   ce.write( x );
    ce.flush();
 }
-
