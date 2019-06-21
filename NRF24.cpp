0855dfce30549e6ae0a9e01fd63c9ab79dc2d0a6
Mennovdjagt <43231296+Mennovdjagt@users.noreply.github.com>
2019-06-21 06:48:49 -0700

    fixed bug
    Apparently I forgot that you cant change a bit that is 1 by using an or ( | that one ) to a zero, so I fixed that bug by putting a 1 on that place and than using a not ( ~ that sign) and instead of using a or with the value of the register I now use an and ( & ), so now you are able to get the values to 0 if needed.

--- NRF24.cpp
+++ NRF24.cpp
@@ -47,7 +47,7 @@ void NRF24::start(){
 
    hwlib::wait_ms( 100 );
 
-   write_register( RF_SETUP, 0xFF & RF24_250KBPS );
+   write_register( RF_SETUP, ( read_register(RF_SETUP) | ( 1 << RF_DR_LOW  ) ) & ~( 1 << RF_DR_HIGH ) );
 
    write_register( FEATURE, 0 );
    write_register( DYNPD, 0 );
@@ -59,7 +59,7 @@ void NRF24::start(){
 
    powerup();
 
-   write_register( CONFIG, read_register(CONFIG) | ( 0 << PRIM_RX ) );
+   write_register( CONFIG, read_register(CONFIG) & ~( 1 << PRIM_RX ) );
 }
 
 void NRF24::write_pipe(uint8_t value){
@@ -78,7 +78,7 @@ void NRF24::read_pipe(){
 void NRF24::powerUp_tx(){
    set_ce( 0 );
    powerup();
-   write_register( CONFIG, read_register(CONFIG) | ( 0 << PRIM_RX ) );
+   write_register( CONFIG, read_register(CONFIG) & ~( 1 << PRIM_RX ) );
    set_ce( 1 );
    write_register( STATUS, ( 1 << RX_DR) |( 1 << TX_DS ) | ( 1 << MAX_RT ) );
 }
@@ -94,7 +94,7 @@ void NRF24::powerUp_rx(){
 void NRF24::powerDown_rx(){
    set_ce( 0 );
    powerdown();
-   write_register( CONFIG, read_register(CONFIG) | ( 0 << PRIM_RX ) );
+   write_register( CONFIG, read_register(CONFIG) & ~( 1 << PRIM_RX ) );
    powerdown();
 }
 
@@ -104,7 +104,7 @@ void NRF24::powerup(){
 
 void NRF24::powerdown(){
    set_ce( 0 );
-   write_register( CONFIG, read_register(CONFIG) & ( 0 << PWR_UP ) );
+   write_register( CONFIG, read_register(CONFIG) & ~( 1 << PWR_UP ) );
 }
 
 void NRF24::flush_rx(){
