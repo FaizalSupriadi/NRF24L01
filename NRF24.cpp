cd6ce558db44988480a5b455a7dbacb0dbae77d0
Mennovdjagt <43231296+Mennovdjagt@users.noreply.github.com>
2019-06-25 11:52:42 +0200

    changed some functions
    I have added something in the function start()
    I also changed some other functions so I can transmit easier

--- NRF24.cpp
+++ NRF24.cpp
@@ -5,6 +5,12 @@
 NRF24::NRF24( hwlib::spi_bus & bus, hwlib::pin_out & ce, hwlib::pin_out & csn ):
    bus( bus ), ce( ce ), csn( csn ), payload_size( 1 )
 {} 
+
+void NRF24::transfer( uint8_t reg ){
+   set_csn(0);
+   bus.transaction( hwlib::pin_out_dummy ).write( reg );
+   set_csn(1);
+}
 	
 uint8_t NRF24::read_register( uint8_t reg ){
    set_csn(0);
@@ -46,12 +52,17 @@ void NRF24::start(){
 
    hwlib::wait_ms( 100 );
 
-   setDataRate(0);
+   setDataRate(1);
+
+   transfer( 0x50 );
+   transfer( 0x73 );
 
    write_register( FEATURE, 0 );
    write_register( DYNPD, 0 );
 
    write_register( STATUS, ( 1 << RX_DR) |( 1 << TX_DS ) | ( 1 << MAX_RT ) );
+
+   setChannel( 100 );
 
    flush_rx();
    flush_tx();
@@ -62,10 +73,10 @@ void NRF24::start(){
 }
 
 void NRF24::write_pipe( uint8_t value ){
-   write_register( RX_ADDR_P1, value );
+   write_register( RX_ADDR_P0, value );
    write_register( TX_ADDR, value );
 
-   write_register( RX_PW_P1, payload_size );
+   write_register( RX_PW_P0, payload_size );
 }
 
 void NRF24::read_pipe(){
@@ -95,7 +106,7 @@ void NRF24::powerDown_rx(){
    set_ce( 0 );
    powerdown();
    write_register( CONFIG, read_register(CONFIG) & ~( 1 << PRIM_RX ) );
-   write_register( EN_RXADDR, read_register( EN_RXADDR ) | ERX_P1 );    //testing purpose (does stand in other function, but I am not sure what they meant in the datasheets so I try it out)
+   write_register( EN_RXADDR, read_register( EN_RXADDR ) | ERX_P0 );    //testing purpose (does stand in other function, but I am not sure what they meant in the datasheets so I try it out)
    powerup();
 }
 
@@ -183,9 +194,23 @@ void NRF24::write( uint8_t value ){
    set_ce( 0 );
 }
 
+uint8_t NRF24::read(){
+   set_ce( 1 );
+   uint8_t result = read_payload();
+   hwlib::wait_ns( 10000 );
+   set_ce( 0 );
+   return result;
+}
+
 void NRF24::write_payload( uint8_t value ){
-   set_csn(0);
-   bus.transaction( hwlib::pin_out_dummy ).write( ( W_TX_PAYLOAD_NOACK ) );
-   bus.transaction( hwlib::pin_out_dummy ).write( value );
-   set_csn(1);
+   transfer( ( W_TX_PAYLOAD_NOACK ) );
+   transfer( value );
+}
+
+uint8_t NRF24::read_payload(){
+   transfer( R_RX_PAYLOAD );
+   set_csn( 0 );
+   uint8_t result = bus.transaction( hwlib::pin_out_dummy ).read_byte();
+   set_csn( 1 );
+   return result;
 }
