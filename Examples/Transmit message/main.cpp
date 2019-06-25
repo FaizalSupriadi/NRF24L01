be9ff7957134b883ed9c5d24956b7492b243f2f8
Mennovdjagt <43231296+Mennovdjagt@users.noreply.github.com>
2019-06-25 13:52:20 +0200

    changed it again
    Because first it did not really worked, not exactly sure why. So I had to change some things in both the transmit and the receive side of the code, the reason why it changed is also because I changed the functions we are using here, for example I set the standard channel that is used to 100 instead of forcing the user to do it.

--- Examples/Transmit message/main.cpp
+++ Examples/Transmit message/main.cpp
@@ -18,13 +18,12 @@ int main( void ){
       sclk, mosi, miso );
 
    auto nrf = NRF24( spi_bus, ce, csn );
-   nrf.start();
-   nrf.setChannel( 100 );                   //sets frequency to 2500 MHz(2400 + 100)
-   nrf.write_pipe( 1 );                     
-   nrf.setOutputPower( 0 );                 //sets the output power to the lowest (-18 dBm) see NRF24.cpp and NRF24.hpp for more information about this
+   nrf.start();                   
+   nrf.write_pipe( 0x01 );                     
+   nrf.setOutputPower( 0 );                 
    nrf.powerDown_rx();
    
    for(;;){
-      nrf.write( 1 );                       //tries to send 1 (can only be 8 bits max for now)
+      nrf.write( 1 );                      
    }
 }
