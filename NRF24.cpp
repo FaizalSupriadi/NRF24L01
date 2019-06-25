582e5e6624706b7b87aa86e3712a2820bcd8aee2
Mennovdjagt <43231296+Mennovdjagt@users.noreply.github.com>
2019-06-25 13:54:21 +0200

    added functions and changed them
    I added a few functions I thought would be usefull ofr the user.
    I also changed the write_pipe and the read_pipe, and some other things because I saw some faults from my side in the code.

--- NRF24.cpp
+++ NRF24.cpp
@@ -7,9 +7,17 @@ NRF24::NRF24( hwlib::spi_bus & bus, hwlib::pin_out & ce, hwlib::pin_out & csn ):
 {} 
 
 void NRF24::transfer( uint8_t reg ){
-   set_csn(0);
+   set_csn( 0 );
    bus.transaction( hwlib::pin_out_dummy ).write( reg );
-   set_csn(1);
+   set_csn( 1 );
+}
+
+uint8_t NRF24::read_byte(){
+   set_csn( 0 );
+   uint8_t value = bus.transaction( hwlib::pin_out_dummy ).read_byte();
+   set_csn( 1 );
+   
+   return value;
 }
 	
 uint8_t NRF24::read_register( uint8_t reg ){
@@ -17,14 +25,15 @@ uint8_t NRF24::read_register( uint8_t reg ){
    bus.transaction( hwlib::pin_out_dummy ).write( R_REGISTER | ( 0x1F & reg ) );
    uint8_t result = bus.transaction( hwlib::pin_out_dummy ).read_byte();
    set_csn(1);
+
    return result;
 }
 	
 void NRF24::write_register( uint8_t reg, uint8_t value ){
-   set_csn(0);
+   set_csn( 0 );
    bus.transaction( hwlib::pin_out_dummy ).write( (W_REGISTER | ( 0x1F & reg )) );
    bus.transaction( hwlib::pin_out_dummy ).write(value);
-   set_csn(1);
+   set_csn( 1 );
 }
 	
 void NRF24::setChannel( uint8_t channel ){
@@ -60,7 +69,7 @@ void NRF24::start(){
    write_register( FEATURE, 0 );
    write_register( DYNPD, 0 );
 
-   write_register( STATUS, ( 1 << RX_DR) |( 1 << TX_DS ) | ( 1 << MAX_RT ) );
+   write_register( STATUS, ( 1 << RX_DR ) |( 1 << TX_DS ) | ( 1 << MAX_RT ) );
 
    setChannel( 100 );
 
@@ -69,7 +78,7 @@ void NRF24::start(){
 
    powerup();
 
-   write_register( CONFIG, read_register(CONFIG) & ~( 1 << PRIM_RX ) );
+   write_register( CONFIG, read_register( CONFIG ) & ~( 1 << PRIM_RX ) );
 }
 
 void NRF24::write_pipe( uint8_t value ){
@@ -79,44 +88,44 @@ void NRF24::write_pipe( uint8_t value ){
    write_register( RX_PW_P0, payload_size );
 }
 
-void NRF24::read_pipe(){
-   write_register( RX_PW_P1 ,payload_size );
+void NRF24::read_pipe( uint8_t address ){
+   write_register( RX_ADDR_P0, address );
+   write_register( RX_PW_P0, payload_size );
    write_register( EN_RXADDR, read_register( EN_RXADDR ) | ( 1 << ERX_P1 ) );
-
-   write_register( RX_PW_P1, payload_size );
 }
 
 void NRF24::powerUp_tx(){
    set_ce( 0 );
    powerup();
-   write_register( CONFIG, read_register(CONFIG) & ~( 1 << PRIM_RX ) );
+   write_register( CONFIG, read_register( CONFIG ) & ~( 1 << PRIM_RX ) );
    set_ce( 1 );
-   write_register( STATUS, ( 1 << RX_DR) |( 1 << TX_DS ) | ( 1 << MAX_RT ) );
+   write_register( STATUS, ( 1 << RX_DR ) |( 1 << TX_DS ) | ( 1 << MAX_RT ) );
 }
 
 void NRF24::powerUp_rx(){
    set_ce( 0 );
    powerup();
-   write_register( CONFIG, read_register(CONFIG) | ( 1 << PRIM_RX ) );
+   write_register( CONFIG, read_register( CONFIG ) | ( 1 << PRIM_RX ) );
    write_register( STATUS, ( 1 << RX_DR ) | ( 1 << TX_DS ) | ( 1 << MAX_RT ) );
+   write_register( EN_RXADDR, ( read_register( EN_RXADDR ) & ~( 1 << ERX_P0 ) ) );
    set_ce( 1 );
 }
 
 void NRF24::powerDown_rx(){
    set_ce( 0 );
    powerdown();
-   write_register( CONFIG, read_register(CONFIG) & ~( 1 << PRIM_RX ) );
+   write_register( CONFIG, read_register( CONFIG ) & ~( 1 << PRIM_RX ) );
    write_register( EN_RXADDR, read_register( EN_RXADDR ) | ERX_P0 );    //testing purpose (does stand in other function, but I am not sure what they meant in the datasheets so I try it out)
    powerup();
 }
 
 void NRF24::powerup(){
-   write_register( CONFIG, read_register(CONFIG) | ( 1 << PWR_UP ) );
+   write_register( CONFIG, read_register( CONFIG ) | ( 1 << PWR_UP ) );
 }
 
 void NRF24::powerdown(){
    set_ce( 0 );
-   write_register( CONFIG, read_register(CONFIG) & ~( 1 << PWR_UP ) );
+   write_register( CONFIG, read_register( CONFIG ) & ~( 1 << PWR_UP ) );
 }
 
 void NRF24::flush_rx(){
@@ -146,9 +155,9 @@ void NRF24::setDataRate( uint8_t speed ){
 hwlib::string<8> NRF24::getDataRate(){
    uint8_t value = ( read_register(RF_SETUP) & 0x28 );
 
-   if( value == 0x20){
+   if( value == 0x20 ){
       return "250Kbps";
-   }else if( value == 0x00){
+   }else if( value == 0x00 ){
       return "1Mbps";
    }else if( value == 0x10 ){
       return "2Mbps";
@@ -159,15 +168,15 @@ hwlib::string<8> NRF24::getDataRate(){
 
 void NRF24::setOutputPower( uint8_t value ){
    if( value == 0 ){
-      write_register( RF_SETUP, read_register( RF_SETUP ) & ~( 1 <<  RF_PWR_HIGH | 1 << RF_PWR_LOW ));
+      write_register( RF_SETUP, read_register( RF_SETUP ) & ~( 1 <<  RF_PWR_HIGH | 1 << RF_PWR_LOW ) );
    }else if( value == 1 ){
       write_register( RF_SETUP, ( read_register( RF_SETUP ) | 1 << RF_PWR_LOW ) & ~( 1 <<  RF_PWR_HIGH ) );
    }else if( value == 2 ){
-      write_register( RF_SETUP, ( read_register( RF_SETUP ) |  1 <<  RF_PWR_HIGH ) & ~( 1 << RF_PWR_LOW ));
+      write_register( RF_SETUP, ( read_register( RF_SETUP ) |  1 <<  RF_PWR_HIGH ) & ~( 1 << RF_PWR_LOW ) );
    }else if( value == 3 ){
-      write_register( RF_SETUP, read_register( RF_SETUP ) | ( 1 <<  RF_PWR_HIGH | 1 << RF_PWR_LOW ));
+      write_register( RF_SETUP, read_register( RF_SETUP ) | ( 1 <<  RF_PWR_HIGH | 1 << RF_PWR_LOW ) );
    }else{
-      write_register( RF_SETUP, read_register( RF_SETUP ) & ~( 1 <<  RF_PWR_HIGH | 1 << RF_PWR_LOW ));
+      write_register( RF_SETUP, read_register( RF_SETUP ) & ~( 1 <<  RF_PWR_HIGH | 1 << RF_PWR_LOW ) );
    }
 }
 
@@ -195,10 +204,9 @@ void NRF24::write( uint8_t value ){
 }
 
 uint8_t NRF24::read(){
-   set_ce( 1 );
    uint8_t result = read_payload();
-   hwlib::wait_ns( 10000 );
-   set_ce( 0 );
+   write_register( STATUS, ( 1 << RX_DR ) | ( 1 << MAX_RT ) | ( 1 << TX_DS ) );
+
    return result;
 }
 
@@ -209,8 +217,7 @@ void NRF24::write_payload( uint8_t value ){
 
 uint8_t NRF24::read_payload(){
    transfer( R_RX_PAYLOAD );
-   set_csn( 0 );
-   uint8_t result = bus.transaction( hwlib::pin_out_dummy ).read_byte();
-   set_csn( 1 );
+   uint8_t result = read_byte();
+
    return result;
 }
