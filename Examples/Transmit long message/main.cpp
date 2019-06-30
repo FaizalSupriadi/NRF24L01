ddadbdfc463b409c51abbb9551bfe3dfc6bf59ba
Mennovdjagt <43231296+Mennovdjagt@users.noreply.github.com>
2019-06-30 16:03:49 +0200

    Update main.cpp

---
+++ Examples/Transmit long message/main.cpp
@@ -0,0 +1,33 @@
+#include "hwlib.hpp"
+#include "NRF24.hpp"
+#include "NRF24_register.hpp"
+
+int main( void ){
+
+   namespace target = hwlib::target;
+
+   auto sclk = target::pin_out( target::pins::d9 );
+   auto mosi = target::pin_out( target::pins::d12 );
+   auto miso = target::pin_in( target::pins::d13 );
+   auto csn  = target::pin_out( target::pins::d11 );
+   auto ce   = target::pin_out( target::pins::d10 );
+
+   hwlib::wait_ms( 100 );
+
+   auto spi_bus = hwlib::spi_bus_bit_banged_sclk_mosi_miso( 
+      sclk, mosi, miso );
+
+   uint8_t address[5] = { 0x00, 0x00, 0x00, 0x00, 0x01 };
+
+   auto nrf = NRF24( spi_bus, ce, csn );
+   nrf.start();                   
+   nrf.write_pipe( address );
+   nrf.powerDown_rx();
+
+   uint8_t value[18] = { 'h', 'a', 'l', 'l', 'o', ' ', 'i', 'k', ' ', 'b', 'e', 'n', ' ', 'm', 'e', 'n', 'n', 'o' };
+   uint8_t len = 18;
+   
+   for(;;){
+      nrf.writeLong( value, len );
+   }
+}
