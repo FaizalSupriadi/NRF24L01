cb181403f09179617a3a66bda72b6399f312f76e
Mennovdjagt <43231296+Mennovdjagt@users.noreply.github.com>
2019-07-02 15:47:16 +0200

    added documentation

--- Examples/Transmit long message/main.cpp
+++ Examples/Transmit long message/main.cpp
@@ -14,22 +14,23 @@ int main( void ){
 
    hwlib::wait_ms( 100 );
 
-   auto spi_bus = hwlib::spi_bus_bit_banged_sclk_mosi_miso( 
+   auto spi_bus = hwlib::spi_bus_bit_banged_sclk_mosi_miso(          //creates a SPI object
       sclk, mosi, miso );
 
-   uint8_t address[5] = { 0x00, 0x00, 0x00, 0x00, 0x01 };
+   uint8_t address[5] = { 0x00, 0x00, 0x00, 0x00, 0x01 };            //the address on which we are going to send data
 
-   auto nrf = NRF24( spi_bus, ce, csn );
-   nrf.start();                   
-   nrf.write_pipe( address );
-   nrf.powerDown_rx();
+   auto nrf = NRF24( spi_bus, ce, csn );                             //creates a object of nrf24l01+
+   nrf.start();                                                      //sets default values into the register of the rf chip
+   nrf.write_pipe( address );                                        //sets the pipe, address and payload size we are going to use 
+   nrf.powerDown_rx();                                               //powers up TX mode
 
-   uint8_t value[18] = { 'h', 'a', 'l', 'l', 'o', ' ', 'i', 'k', ' ', 'b', 'e', 'n', ' ', 'm', 'e', 'n', 'n', 'o' };
-   uint8_t len = 18;
+   //A bigger value than our payload (which is 5 bytes and not 18 bytes)
+   uint8_t value[18] = { 'H', 'a', 'l', 'l', 'o', ' ', 'i', 'k', ' ', 'b', 'e', 'n', ' ', 'M', 'e', 'n', 'n', 'o' };
+   uint8_t len = 18;                       //the length of our value in bytes
    
-   for(;;){
-      hwlib::wait_ms( 200 );
+   for(;;){                                //infinity loop
+      hwlib::wait_ms( 200 );               //wait 200 miliseconds
       
-      nrf.writeLong( value, len );
+      nrf.writeLong( value, len );         //transmits a longer value than the payload size is
    }
 }
