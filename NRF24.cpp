65ab73ec3d45d7706e82e95f61612c861d619ee1
Mennovdjagt <43231296+Mennovdjagt@users.noreply.github.com>
2019-07-04 14:46:59 +0200

    Update NRF24.cpp

--- NRF24.cpp
+++ NRF24.cpp
@@ -1,3 +1,11 @@
+// File      : NRF24.cpp
+// Part of   : NRF24L01+ library
+// Copyright : menno.vanderjagt@student.hu.nl 2019
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at 
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #include "NRF24.hpp"
 #include "NRF24_register.hpp"
 #include "hwlib.hpp"
